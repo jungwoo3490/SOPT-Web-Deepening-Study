20ee14ef5d327088c182d5e3264462a5beb6f573
jungwoo3490 <jungwoo3490@naver.com>
2023-10-27 03:50:02 +0900

    [1주차] pnpm & Global Style

---
+++ Week1/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { RedButton } from './styles/GlobalStyle';
+
+function App() {
+  return (
+    <div>
+        <h1>h1태그입니다</h1>
+        <h3>h3태그입니다</h3>
+        <h5>h3태그입니다</h5>
+        <p>p태그입니다</p>
+        <RedButton>button 입니다</RedButton>
+        <a href="/">a태그입니다</a>
+    </div>
+  );
+}
+
+export default App;
